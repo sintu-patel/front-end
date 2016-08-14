49b070dd083d61419a4f0ab553f99aefab92f8dd
Sintu Singh <ssingh172@sapient.com>
2016-08-14 13:55:22 +0100

    game

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,26 @@ gulp.task('compileReactSASS', function() {
 		.pipe(gulp.dest('reactjs/assets/css/'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch(['reactjs/**/*'], ['compileReactSASS']);
+gulp.task('compileGameSASS', function() {
+	gulp.src('game/sass/app.scss')
+		.pipe(compass({
+			css: 'game/assets/css/',
+			sass: 'game/sass/'
+		}))
+		.pipe(gulp.dest('game/assets/css/'));
 });
 
-gulp.task('default', ['compileReactSASS']);
+gulp.task('compileAngularSASS', function() {
+	gulp.src('angularjs/sass/app.scss')
+		.pipe(compass({
+			css: 'angularjs/assets/css/',
+			sass: 'angularjs/sass/'
+		}))
+		.pipe(gulp.dest('angularjs/assets/css/'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(['reactjs/**/*', 'game/**/*', 'angularjs/**/*'], ['compileReactSASS', 'compileGameSASS', 'compileAngularSASS']);
+});
+
+gulp.task('default', ['compileReactSASS', 'compileGameSASS', 'compileAngularSASS']);
