a3ef2ccb7466a71a518639d2097cb4bbfed5682e
Sintu Singh <ssingh172@sapient.com>
2016-08-19 00:07:06 +0100

    flexbox

--- raw/pages/flexbox/flexbox.js
+++ raw/pages/flexbox/flexbox.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-  var mountData = function(cssClass) {
-    return pageData.main[cssClass];
-  };
-
-  var $page = $('.page');
-  var $html = '<p>' + mountData('paragraph') + '</p>'
-  $page.find('.paragraph').html($html);
+  mountData('paragraph');
 });
+
+// Change content of an element with respective to class in data
+var mountData = function(cssClass) {
+  var text = pageData.main[cssClass];
+  var $page = $('.page');
+  $page.find('.' + cssClass).html(text);
+};
