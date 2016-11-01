985ef3a3b5ddf2f0067cf608bf901884e3536e1f
Sintu Singh <ssingh172@sapient.com>
2016-11-01 22:08:25 +0000

    Game

--- raw/pages/html-5-shot-ball/app.js
+++ raw/pages/html-5-shot-ball/app.js
@@ -25,44 +25,82 @@ var clearRectObj = function (x, y, w, h) {
   ctx.fillRect(x, y, w, h);
 }
 
+var targetCr = {
+  x: 0,
+  y: 0
+};
+
 var count = 0;
 var leftPos = 0;
 var intervalID = window.setInterval(function(){
-  clearRectObj(0, 0, leftPos, 110);
+  clearRectObj(leftPos - 110, 0, leftPos, 110);
   leftPos = leftPos + 1;
   drawBall(leftPos, 60)
+  targetCr = {
+    x: leftPos,
+    y: 110
+  };
   if(leftPos >= 500) {
     leftPos = 0;
   }
-}, 10);
+}, 1);
 
 
 // Draw object
 var drawObject = function(y) {
-  ctx.fillStyle = $colorGreenLight;
-  ctx.fillRect(200, y, 40, 40);
+  ctx.fillStyle = 'red';
+  ctx.fillRect(200, y, 10, 50);
 }
+
+var hitTarget = function(hit) {
+
+  if(((hit.x < targetCr.x) && (targetCr.x - hit.x <= 10)) && ((hit.y > targetCr.y) && (hit.y - targetCr.y) <= 20)) {
+    clearInterval(intervalID);
+  }
+};
 
 var startTarget = function() {
   var countObj = 0;
-  var yPos = 360;
+  var yPos = 350;
   var intervalOB = window.setInterval(function(){
     countObj = countObj + 1;
     yPos = yPos - countObj;
-    clearRectObj(200, yPos + 10, 40, 40);
+    clearRectObj(200, yPos + 10, 10, 65);
     drawObject(yPos);
-    if(yPos <= 90) {
+    hitTarget({x: 200, y: yPos});
+    if(yPos <= 60) {
       clearInterval(intervalOB);
     }
   }, 10);
 }
 
-drawObject(400);
+var drawButton = function(y) {
+  var radius = 20;
+  ctx.beginPath();
+  ctx.arc(220, y, radius, 0, 2 * Math.PI, false);
+  ctx.fillStyle = $colorGreenLight;
+  ctx.fill();
+}
+
+drawButton(460);
 app.onclick = function(e) {
   var x = e.clientX - canvasLeft;
   var y = e.clientY - canvasTop;
 
-  if(x > 200 && x < 240 && y > 400 && y < 440) {
+  if(x > 200 && x < 240 && y > 440 && y < 480) {
     startTarget();
+  }
+};
+
+app.onmousemove = function(e) {
+  var x = e.clientX - canvasLeft;
+  var y = e.clientY - canvasTop;
+
+  if(x > 200 && x < 240 && y > 440 && y < 480) {
+    app.style.cursor = 'pointer';
+  }
+
+  else {
+    app.style.cursor = 'move';
   }
 };
