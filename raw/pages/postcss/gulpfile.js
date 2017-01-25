24623b87ee310543f44dad73eef23e9fa1fa3b46
Sintu Singh <ssi139@sapient.com>
2017-01-25 20:39:21 +0530

    'app'

--- raw/pages/postcss/gulpfile.js
+++ raw/pages/postcss/gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
-gulp.task('css', function () {
-    return gulp.src('dev/style/*.css')
-        .pipe( sourcemaps.init() )
-        .pipe( postcss([ require('precss'), require('autoprefixer') ]) )
-        .pipe( sourcemaps.write('.') )
-        .pipe( gulp.dest('build/') );
+
+// Plugins
+var precss = require('precss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('runPostCss', function () {
+	return gulp.src('dev/style/*.css')
+		.pipe(sourcemaps.init())
+		.pipe(postcss([precss, autoprefixer]))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('build/'));
 });
+
+gulp.task('default', ['runPostCss']);
